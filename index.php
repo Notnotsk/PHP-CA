7146afb0e869459f1b05291a657deb2f2ad244a6
Scott Knoll <scknoll88@gmail.com>
2023-02-28 15:51:49 -0600

    more return statements

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
-  
-  function printStringReturnNumber()
-  {
-    echo "Page not found\n";
-    return 404;
-  }
-  $my_num = printStringReturnNumber();
-  echo $my_num;
+
+function notFound()
+{
+  echo "ERROR: Page not found!\n";
+  return 404;
+}
+
+function greetLearner()
+{
+  echo "Hello, Learner!\n";
+  echo "I hope you're (still) enjoying PHP!\n";
+  echo "Love, Codecademy\n";
+  return "<3";
+}
+
+$error = notFound(); 
+$heart = greetLearner();
+
+echo "I received a $error, but it's ok because I also received $heart.";
