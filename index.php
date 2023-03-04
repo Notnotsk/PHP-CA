4bbf3c2c11e3aed9f1c05052492b5ea2c088b2ca
Scott Knoll <scknoll88@gmail.com>
2023-03-03 22:34:02 -0600

    parameters

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
-
 // Write your code below:
-  
-function createVacuum()
-  {
-  }
-  
-  
-  
-  
-  
-  
-  
-echo createVacuum() * 10;
+function increaseEnthusiasm($name)
+{
+  return $name . "!";
+}
+echo increaseEnthusiasm("You got it");
+
+function repeatThreeTimes($name)
+{
+  return $name . $name . $name;
+}
+echo repeatThreeTimes(" There's no place like home!");
